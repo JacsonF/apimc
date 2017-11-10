dbffbb6b415720b8ea93928ee50571d224ce9539
Jacson Ferreira <jacsonferreira1994@gmail.com>
2017-11-10 08:07:20 -0200

    Projeto criado

---
+++ src/main/java/com/jacsonferreira/apimc/ApimcApplication.java
@@ -0,0 +1,12 @@
+package com.jacsonferreira.apimc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimcApplication.class, args);
+	}
+}
