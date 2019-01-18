3a4fd8175c09a77f3fe1d297483aaa9a9cb7d85e
Jacson Ferreira Andrade <jacsonferreira1994@gmail.com>
2019-01-18 07:13:11 -0200

    Inserindo um novo Cliente com POST

--- src/main/java/com/jacsonferreira/apimc/resources/ClientResource.java
+++ src/main/java/com/jacsonferreira/apimc/resources/ClientResource.java
@@ -1,5 +1,6 @@
 package com.jacsonferreira.apimc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,55 +15,69 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.jacsonferreira.apimc.domain.Client;
 import com.jacsonferreira.apimc.dto.ClientDTO;
+import com.jacsonferreira.apimc.dto.ClientNewDTO;
 import com.jacsonferreira.apimc.services.ClientService;
 
 @RestController
-@RequestMapping(value="/clients")
+@RequestMapping(value = "/clients")
 public class ClientResource {
 	@Autowired
 	private ClientService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Client> find(@PathVariable Integer id) {
-		Client obj = service.Find(id);	
+		Client obj = service.Find(id);
 		return ResponseEntity.ok(obj);
 	}
 
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@PathVariable Integer id, @Valid  @RequestBody ClientDTO client) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody ClientDTO client) {
 		Client obj = service.fromDTO(client);
 		obj.setId(id);
-		obj= service.update(obj);	 
-		
+		obj = service.update(obj);
+
 		return ResponseEntity.noContent().build();
 	}
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<Void> delete (@PathVariable Integer id){
-		
-	service.delete(id);
-	return ResponseEntity.noContent().build();
-		
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
 	}
-	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<ClientDTO>>findall(){
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClientDTO>> findall() {
 		List<Client> categories = service.findAll();
-		
-		List<ClientDTO> categoryDTOs = categories.stream().map( obj -> new ClientDTO(obj)).collect(Collectors.toList()); 
+
+		List<ClientDTO> categoryDTOs = categories.stream().map(obj -> new ClientDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(categoryDTOs);
 	}
-	@RequestMapping(value="/page",method=RequestMethod.GET)
-	public ResponseEntity<Page<ClientDTO>>findPage(
-			@RequestParam(value="page",defaultValue="0") Integer page,
-			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage,
-			@RequestParam(value="orderBy",defaultValue="nome")String orderBy,
-			@RequestParam(value="direction", defaultValue="ASC")String direction){
-		
-		Page<Client> clients= service.findPage(page, linesPerPage, orderBy, direction);
-		
-		Page<ClientDTO> clientDTOs = clients.map( obj -> new ClientDTO(obj)); 
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClientDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Client> clients = service.findPage(page, linesPerPage, orderBy, direction);
+
+		Page<ClientDTO> clientDTOs = clients.map(obj -> new ClientDTO(obj));
 		return ResponseEntity.ok().body(clientDTOs);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClientNewDTO objDTO) {
+		Client obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
 }
--- src/main/java/com/jacsonferreira/apimc/services/ClientService.java
+++ src/main/java/com/jacsonferreira/apimc/services/ClientService.java
@@ -9,50 +9,93 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.jacsonferreira.apimc.domain.Address;
+import com.jacsonferreira.apimc.domain.City;
 import com.jacsonferreira.apimc.domain.Client;
+import com.jacsonferreira.apimc.domain.enums.ClientType;
 import com.jacsonferreira.apimc.dto.ClientDTO;
+import com.jacsonferreira.apimc.dto.ClientNewDTO;
+import com.jacsonferreira.apimc.repositories.AddressRepository;
+import com.jacsonferreira.apimc.repositories.CityRepositoriy;
 import com.jacsonferreira.apimc.repositories.ClientRepository;
 import com.jacsonferreira.apimc.services.exceptions.DataIntegrityExeption;
 import com.jacsonferreira.apimc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ClientService {
-	
+
 	@Autowired
 	private ClientRepository repo;
-	
+	@Autowired
+	private CityRepositoriy cityRepository;
+	@Autowired
+	private AddressRepository addressRepository;
+
+	public Client insert(Client client) {
+		client.setId(null);
+		client = repo.save(client);
+		addressRepository.save(client.getAddresses());
+		return client;
+	}
+
 	public Client Find(Integer id) {
 		Client obj = repo.findOne(id);
-		if (obj ==null) {
-			throw new ObjectNotFoundException("objeto não encontrado id: " +id +" , tipo: " +Client.class.getName());
+		if (obj == null) {
+			throw new ObjectNotFoundException("objeto não encontrado id: " + id + " , tipo: " + Client.class.getName());
 		}
 		return obj;
 	}
+
 	public Client update(Client client) {
-		 Client newObj= Find(client.getId());
-		 updateData(newObj, client);
+		Client newObj = Find(client.getId());
+		updateData(newObj, client);
 		return repo.save(newObj);
 	}
+
 	public void delete(Integer id) {
 		Find(id);
 		try {
-		repo.delete(id);	
-		}catch (DataIntegrityViolationException  e) {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityExeption("Can not delete a client that has orders");
 		}
 	}
+
 	public List<Client> findAll() {
 		return repo.findAll();
 	}
-	public Page<Client> findPage(Integer page , Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest = new PageRequest(page, linesPerPage,Direction.valueOf(direction), orderBy);
+
+	public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
+
 	public Client fromDTO(ClientDTO clientDTO) {
-		return new Client(clientDTO.getId(), clientDTO.getName(),clientDTO.getEmail(),null,null);
-	}	
+		return new Client(clientDTO.getId(), clientDTO.getName(), clientDTO.getEmail(), null, null);
+	}
+
+	public Client fromDTO(ClientNewDTO clientDTO) {
+		Client client = new Client(null, clientDTO.getName(), clientDTO.getEmail(), clientDTO.getCpfOrCnpj(),
+				ClientType.toEnum(clientDTO.getClientType()));
+		
+		City city = cityRepository.findOne(clientDTO.getCityId());
+		
+		Address address = new Address(null, clientDTO.getPatio(), clientDTO.getNumber(), clientDTO.getComplement(),
+				clientDTO.getDistrict(), clientDTO.getCep(), client, city);
+
+		client.getAddresses().add(address);
+		client.getPhones().add(clientDTO.getPhone1());
+		if (clientDTO.getPhone2() != null) {
+			client.getPhones().add(clientDTO.getPhone2());
+		}
+		if (clientDTO.getPhone3() != null) {
+			client.getPhones().add(clientDTO.getPhone3());
+		}
+		return client;
+	}
+
 	private void updateData(Client newObj, Client obj) {
-		newObj.setName( obj.getName());
+		newObj.setName(obj.getName());
 		newObj.setEmail(obj.getEmail());
 	}
 }
